fd921eedf9139fdc25327839cad70ae6bdc547a8
Shriyam Singh <121627937+Rockshri@users.noreply.github.com>
2025-06-16 23:39:19 +0530

    Initial Commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,25 +1,19 @@
 import dotenv from "dotenv";
-dotenv.config({path:"../"});
+dotenv.config({ path: "../../.env" });
 import pg from "pg";
 const { Pool } = pg;
-// import process from "process";
+import process from "process";
 
 // console.log("env keys:", Object.keys(process.env));
 // console.log("DB_USER:", process.env.DB_USER);
 
 //database connection
 const dbConnect = new Pool({
-  host: "localhost",
-  user: "postgres",
-  port: 5432,
-  password: "Shriyam@20",
-  database: "UniwareDB",
-
-    // host: process.env.DB_HOST || "",
-    // user: process.env.DB_USER || "",
-    // port: process.env.DB_PORT || "",
-    // password: process.env.DB_PASSWORD || "",
-    // database: process.env.DB_NAME || ""
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 export async function dbConnectionHandler() {
